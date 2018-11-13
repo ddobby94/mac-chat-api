d6bb9d3d4f2bf63b3087d37f7ffb298105ae00ba
ddobby94 <dobisz_david@windowslive.com>
2018-11-13 16:06:42 +0100

    change config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
-import mongodb from 'mongodb';
 
 export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  // "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb://david:ASDasd123@ds261253.mlab.com:61253/dobby-kotlin-chat",
   "bodyLimit": "100kb"
 }
